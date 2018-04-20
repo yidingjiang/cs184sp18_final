5a04bf352405bc810afb9a74e55b6564897fa535
YiDing Jiang <yidinggjiangg@gmail.com>
2018-04-19 22:16:38 -0700

    jk better surprsie

--- src/fluidSimulator.cpp
+++ src/fluidSimulator.cpp
@@ -213,7 +213,8 @@ void FluidSimulator::drawContents() {
   shader.setUniform("viewProjection", viewProjection);
   shader.setUniform("light", Vector3f(0.5, 2, 2));
   shader.setUniform("in_color", color);
-  shader.setUniform("particle_size", 6.*(7.-camera.r));
+  // shader.setUniform("particle_size", 6.*(7.-camera.r));
+  shader.setUniform("particle_size", 35./camera.r);
 
   glBindVertexArray(positionsVAO);
   glBindBuffer(GL_ARRAY_BUFFER, positionsVBO);
